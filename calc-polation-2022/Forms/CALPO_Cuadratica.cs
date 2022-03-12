678702f25ae081f4515654fe81bb28965edf3657
JohnyDeCoder <johnymora251@gmail.com>
2022-03-12 17:54:48 -0600

    Added label version and fix some bugs.

--- calc-polation-2022/Forms/CALPO_Cuadratica.cs
+++ calc-polation-2022/Forms/CALPO_Cuadratica.cs
@@ -764,7 +764,8 @@ namespace calc_polation_2022
             {
                 // Si encuentra un TextBox con únicamente un - o punto o ambos juntos.
                 if (oControls is TextBox & oControls.Text == "-" || oControls.Text == "." ||
-                    oControls.Text.Contains("-.") || oControls.Text.Contains(".-"))
+                    oControls.Text.Contains("-.") || oControls.Text.Contains(".-") ||
+                    oControls.Text.EndsWith("-"))
                 {
                     txtContains = true;
                 }
